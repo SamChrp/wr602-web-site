5f739e09600f4207c9a6453181a019b04820e58a
Sam <100703206+SamChrp@users.noreply.github.com>
2024-05-28 15:04:23 +0200

    feat : login management

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -53,8 +53,7 @@ class RegistrationController extends AbstractController
             );
 
             // do anything else you need here, like send an email
-
-            return $security->login($user, 'form_login', 'main');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
@@ -80,6 +79,8 @@ class RegistrationController extends AbstractController
         // validate email confirmation link, sets User::isVerified=true and persists
         try {
             $this->emailVerifier->handleEmailConfirmation($request, $user);
+            $this->addFlash('success', 'Your email address has been verified.');
+            return $this->redirectToRoute('app_login');
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
 
@@ -87,8 +88,5 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
-
-        return $this->redirectToRoute('app_register');
     }
 }
