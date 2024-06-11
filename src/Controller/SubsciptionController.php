598e2bef83f323aa0850d9e3389ed8a7f8a95d7e
Sam <100703206+SamChrp@users.noreply.github.com>
2024-06-11 17:34:05 +0200

    feat : subcription management

--- src/Controller/SubsciptionController.php
+++ src/Controller/SubsciptionController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Subscription;
+use App\Entity\User;
 use App\Repository\SubscriptionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -21,5 +24,25 @@ class SubsciptionController extends AbstractController
         return $this->render('subsciption/index.html.twig', [
             'subscriptions' => $subscriptions,
         ]);
+    }
+
+    #[Route('/subsciption/user/{subscription}', name: 'app_subsciption_user')]
+    public function subsciptionUser
+    (
+        Subscription $subscription,
+        EntityManagerInterface $entityManager
+    )
+    : Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            $this->addFlash('error', 'Vous devez être connecté pour vous abonner à une offre.');
+            return $this->redirectToRoute('app_subsciption');
+        }
+
+        $user->setSubscription($subscription);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_subsciption');
     }
 }
